23ddddc65037bc92a326e4352e92f9cdc0ddc61a
Illia Bondar <kava.kav.23@gmail.com>
2024-05-13 15:05:01 +0200

    09.05.2024 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductCardComponent, ProductsPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/products-page/products-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProductCardComponent } from '../product-card/product-card.component';
+@Component({
+  selector: 'app-products-page',
+  standalone: true,
+  imports: [ProductCardComponent],
+  templateUrl: './products-page.component.html',
+  styleUrl: './products-page.component.css'
+})
+export class ProductsPageComponent {
+
+}
